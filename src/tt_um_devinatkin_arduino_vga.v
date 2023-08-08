8d846fa8deb94f2eac6de73e36efaadee7832062
Devin <devinatkin@gmail.com>
2023-08-07 21:44:23 -0600

    corrected dumb variable name error

--- src/tt_um_devinatkin_arduino_vga.v
+++ src/tt_um_devinatkin_arduino_vga.v
@@ -24,7 +24,7 @@ module tt_um_devinatkin_arduino_vga
     wire [1:0] blue_pixel;
 
     reg [31:0] configuration;
-    wire [7:0] revieved_data;
+    wire [7:0] received_data;
     wire [11:0] rand_num;
 
     // Instantiate vga_timing_gen
@@ -84,7 +84,7 @@ module tt_um_devinatkin_arduino_vga
         .miso(uio_out[0]),
         .sclk(ui_in[1]),
         .config_data(configuration),        // Data to be used by the device
-        .recieved_data(revieved_data)        // Data recieved from the device
+        .recieved_data(received_data)        // Data recieved from the device
     );
 
     always @(posedge clk) begin
