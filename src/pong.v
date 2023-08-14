7bf3aa2683c2276d66fefb35f4490bbf11d5e79c
Devin <devinatkin@gmail.com>
2023-08-13 21:55:54 -0600

    bouncing logic

--- src/pong.v
+++ src/pong.v
@@ -32,7 +32,21 @@ always @(posedge clk) begin
         ball_y <= ball_y + ball_dy;
         // ball_dx <= ball_dx;
         // ball_dy <= ball_dy;
-        // paddle_y <= paddle_y;
+        paddle_y <= paddle_y + btn_down - btn_up;
+
+        if (ball_x == 0) begin // left wall collision
+            ball_dx <= -ball_dx;
+        end else if (ball_x == SCREEN_WIDTH - 1) begin // right wall collision
+            ball_dx <= -ball_dx;
+        end else if (ball_y == 0) begin // top wall collision
+            ball_dy <= -ball_dy;
+        end else if (ball_y == SCREEN_HEIGHT - 1) begin // bottom wall collision
+            ball_dy <= -ball_dy;
+        end else if (ball_x == 20 && ball_y >= paddle_y && ball_y <= paddle_y + PADDLE_HEIGHT) begin // paddle collision
+            ball_dx <= -ball_dx;
+        end
+
+        
     end
 end
 
