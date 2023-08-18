0363354756e8e50dcdd5feb50afb823d1726133a
Devin <devinatkin@gmail.com>
2023-08-17 22:13:02 -0600

    adding missing pixel driver

--- src/pong.v
+++ src/pong.v
@@ -50,14 +50,26 @@ module pong (
             ball_dy <= 2'b01;
             paddle_y <= (SCREEN_HEIGHT - PADDLE_HEIGHT) / 2;
         end else begin
-            // Existing code for ball movement and collision
-            // ...
-
-            // Paddle movement using debounced button signals
+            ball_x <= ball_x + ball_dx;
+            ball_y <= ball_y + ball_dy;
+            // ball_dx <= ball_dx;
+            // ball_dy <= ball_dy;
             paddle_y <= paddle_y + btn_down - btn_up;
 
-            // Existing code for pixel output
-            // ...
+            if (ball_x == 0) begin // left wall collision
+                ball_dx <= -ball_dx;
+            end else if (ball_x == SCREEN_WIDTH - 1) begin // right wall collision
+                ball_dx <= -ball_dx;
+            end else if (ball_y == 0) begin // top wall collision
+                ball_dy <= -ball_dy;
+            end else if (ball_y == SCREEN_HEIGHT - 1) begin // bottom wall collision
+                ball_dy <= -ball_dy;
+            end else if (ball_x == 20 && ball_y >= paddle_y && ball_y <= paddle_y + PADDLE_HEIGHT) begin // paddle collision
+                ball_dx <= -ball_dx;
+            end
+
+
+            pixel <= (x >= ball_x - 5 && x <= ball_x + 5 && y >= ball_y - 5 && y <= ball_y + 5) | (x >= 20 && x <= 30 && y >= paddle_y && y <= paddle_y + PADDLE_HEIGHT);
         end
     end
 
