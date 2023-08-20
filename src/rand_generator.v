b19ffd3cdb5737643e16a4f3b91aa9353e767883
Devin <devinatkin@gmail.com>
2023-08-20 17:24:23 -0600

    reduce rand generator

--- src/rand_generator.v
+++ src/rand_generator.v
@@ -4,18 +4,20 @@ module rand_generator (
     input wire clk,
     input wire reset_n,
     input wire enable,      // Active-high enable signal
-    output reg [11:0] rand_num
+    output reg [11:0] rand_num // Reduced to 20-bit output
 );
 
-    reg [23:0] LFSR; // Reduced to 24-bit LFSR
+    reg [19:0] LFSR; // Reduced to 20-bit LFSR
 
     always @(posedge clk) begin
         if (!reset_n) begin
-            LFSR <= 24'h123456; // Initial seed with reduced size
-            rand_num <= 12'b0;
+            LFSR <= 20'h12345; // Initial seed with reduced size
+            rand_num <= 20'b0;
         end else if (enable) begin // Added enable condition here
-            LFSR <= {LFSR[22:0], LFSR[23]^LFSR[22]^LFSR[21]^LFSR[16]}; // New polynomial
-            rand_num <= LFSR[11:0];
+            // Feedback polynomial for 20-bit LFSR
+            // You may want to choose a different polynomial that suits your application
+            LFSR <= {LFSR[18:0], LFSR[19]^LFSR[17]^LFSR[16]^LFSR[15]}; 
+            rand_num <= LFSR[11:0]; // Output the entire 20-bit LFSR
         end
     end
 
