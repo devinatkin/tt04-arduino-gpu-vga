26bc3076568582cd4c39ebdb9850caaef3beb7f7
Devin <devinatkin@gmail.com>
2023-08-19 21:10:18 -0600

    reducing design size

--- src/rand_generator.v
+++ src/rand_generator.v
@@ -6,14 +6,14 @@ module rand_generator (
     output reg [11:0] rand_num
 );
 
-    reg [31:0] LFSR; // Reduced to 32-bit LFSR
+    reg [23:0] LFSR; // Reduced to 24-bit LFSR
 
     always @(posedge clk) begin
         if (!reset_n) begin
-            LFSR <= 32'h12345678; // Initial seed with reduced size
+            LFSR <= 24'h123456; // Initial seed with reduced size
             rand_num <= 12'b0;
         end else begin
-            LFSR <= {LFSR[30:0], LFSR[31]^LFSR[21]^LFSR[1]^LFSR[0]}; // New polynomial
+            LFSR <= {LFSR[22:0], LFSR[23]^LFSR[22]^LFSR[21]^LFSR[16]}; // New polynomial
             rand_num <= LFSR[11:0];
         end
     end
