4646ccc3734efa70cbb9532d9825b55ab7f5b91e
Devin <devinatkin@gmail.com>
2023-08-20 23:28:41 -0600

    further shrinking the debounce

--- src/debounce.v
+++ src/debounce.v
@@ -7,22 +7,22 @@ module debounce(
     output reg button_out  // Debounced button output
 );
 
-    parameter DEBOUNCE_TIME = 4'b1111; // Define debounce time (must be within 8-bit range)
+    parameter DEBOUNCE_TIME = 3'b111; // Define debounce time (must be within 8-bit range)
 
-    reg [3:0] counter; // 5-bit counter to measure the debounce time
+    reg [2:0] counter; // 5-bit counter to measure the debounce time
 
     always @(posedge clk) begin
         if (!rst_n) begin
             button_out <= 1'b0; // Reset debounced button output
-            counter <= 4'd0;    // Reset counter
+            counter <= 3'd0;    // Reset counter
         end else begin
             if (button_in == button_out) begin
-                counter <= 4'd0; // If button state matches debounced state, reset the counter
+                counter <= 3'd0; // If button state matches debounced state, reset the counter
             end else begin
                 if (counter == DEBOUNCE_TIME - 1'd1) begin
                     button_out <= button_in; // If debounce time has passed, update the debounced output
                 end else begin
-                    counter <= counter + 4'd1; // Otherwise, increment the counter
+                    counter <= counter + 3'd1; // Otherwise, increment the counter
                 end
             end
         end
